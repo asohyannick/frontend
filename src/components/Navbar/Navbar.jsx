da3a80070f6493dc3e74bf8efdf84d3be923dd4a
Asoh Yannick <asohyannick3@gmail.com>
2022-12-24 12:02:27 -0800

    Big Nav Section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,71 @@
 import React from 'react'
 import './Navbar.css';
 import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { FaAlignJustify } from 'react-icons/fa';
 const Navbar = () => {
+  const [ navOpen, SetNavOpen ] = useState(false);
   return (
-    <div>
+    <article>
+      <nav className='nav-section-container'>
+        <ul className='nav-ui'>
+          <li className='nav-ui-items'>
+            <Link to='/about'>About</Link>
+          </li>
+          <li className='nav-ui-items'>
+            <Link to='/acess'>Access</Link>
+          </li>
+          <li className='nav-ui-items'>
+            <Link to='/collections'>Collections</Link>
+          </li>
+          <li className='nav-ui-items'>
+            <Link to='/enterPrise'>EnterPrise</Link>
+          </li>
+          <li className='nav-ui-items'>
+            <Link to='/payment'>Payments</Link>
+          </li>
+          <li className='nav-ui-items'>
+            <Link to='/products'>products</Link>
+          </li>
+          <li className='nav-ui-items'>
+            <Link to='/projects'>Projects</Link>
+          </li>
+          <li className='nav-ui-items'>
+            <Link to='/contact'>Contact</Link>
+          </li>
+          <li className='nav-ui-items'>
+            <Link to='/duty'>Duty</Link>
+          </li>
+          <li className='nav-ui-items'>
+            <Link to='/discipline'>Discipline</Link>
+          </li>
+          <li className='nav-ui-items'>
+            <Link to='/accomp'>Accomplishments</Link>
+          </li>
+          <li className='nav-ui-items'>
+            <Link to='/services'>Services</Link>
+          </li>
+          <li className='nav-ui-items'>
+            <button className='navbar-btn'>
+              <Link to='/buttons'>Sign up</Link>
+            </button>
+          </li>
+          <li className='nav-ui-items'>
+            <button className='nav-btn'>
+              <Link to='/buttons'>Login</Link>
+            </button>
+          </li>
+          <li className='nav-ui-items'>
+            <Link to='/cookies'>Cookies</Link>
+          </li>
+        </ul>
+        <button onClick={() => SetNavOpen(!navOpen)}>
+          < FaAlignJustify/>
+        </button>
+      </nav>
+      
       <Link to='/'> Click here to get the Home component.</Link>
-    </div>
+    </article>
   )
 }
 
--- src/pages/Accounts/Account.jsx
+++ src/pages/Accounts/Account.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './Account.css';
+import { Link } from 'react-router-dom';
 const Account = () => {
   return (
-    <div>Account</div>
+    <div>Account
+      <Link to='/'>Go Back To Home</Link>
+    </div>
   )
 }
 
